240cc32cc1dc2ee28d27568f3eade3bf9dae6a8a
chrislopsa <christian_lopez09@hotmail.com>
2024-11-28 00:44:33 -0500

    connection with DB in cloud

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,31 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatesModule } from './dates/dates.module';
+import { ConfigModule } from '@nestjs/config'
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
+import { Date } from './dates/entities/date.entity';
+
+
 
 @Module({
-  imports: [DatesModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env'
+    }),
+    TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: process.env.DB_HOST, 
+        port: parseInt(process.env.DB_PORT, 10),
+        username: process.env.DB_USER , 
+        password: process.env.DB_PASS , 
+        database: process.env.DB_NAME , 
+        entities: [User, Date],  
+        synchronize: true,
+      }),
+  DatesModule,
+  UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/dates/entities/date.entity.ts
+++ src/dates/entities/date.entity.ts
@@ -1,1 +1,19 @@
-export class Date {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('dates') 
+export class Date {
+  @PrimaryGeneratedColumn() 
+  id: number;
+
+  @Column({ type: 'varchar', length: 100 }) 
+  patient: string;
+
+  @Column({ type: 'varchar', length: 150 }) 
+  doctor: string;
+
+  @Column({ type: 'varchar', length: 255 }) 
+  startDate: string;
+
+  @Column({ type: 'varchar', length: 255 }) 
+  endDate: string;
+}
